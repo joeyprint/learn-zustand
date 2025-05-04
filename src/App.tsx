ca1c62ff369c41546a1f84f38919853be7541f9f
Joey Wasin <wasin.ww1402@gmail.com>
2025-05-04 13:32:27 +0700

    feat: try to integrate intp app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+
 import Appbar from "./Components/Appbar";
 import Toolbar from "./Components/ToolBar";
+import { TextField } from "./Components/Bases/Inputs";
 
 import "./BaselineStyles.css";
 import "./Components/Themes/Theme.css";
@@ -12,7 +14,13 @@ function App() {
     <>
       <Appbar />
       <Toolbar />
-
+      <div className="flex flex-direction-column justify-content-start align-items-center">
+        <div className="flex align-items-center">
+          <TextField />
+          <button>Submit</button>
+        </div>
+        <p>Todo List</p>
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
