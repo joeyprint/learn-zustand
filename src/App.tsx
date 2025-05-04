cbd3bcc9dddbf8d06289d35d844860b32e329037
Joey Wasin <wasin.ww1402@gmail.com>
2025-05-04 13:33:01 +0700

    feat: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import Appbar from "./Components/Appbar";
 import Toolbar from "./Components/ToolBar";
 import { TextField } from "./Components/Bases/Inputs";
@@ -8,8 +6,6 @@ import "./BaselineStyles.css";
 import "./Components/Themes/Theme.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Appbar />
@@ -21,17 +17,6 @@ function App() {
         </div>
         <p>Todo List</p>
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
